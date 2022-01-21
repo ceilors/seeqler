8400b78667552a0cc8df6d3199ae55913ce85e93
Vova Abdrakhmanov <369565@gmail.com>
2022-01-21 12:18:45 +0300

    feat: interactivity

--- seeqler/__main__.py
+++ seeqler/__main__.py
@@ -10,7 +10,7 @@ def main(connection_string: str):
     inspector = sa.inspect(engine)
     schemas = inspector.get_schema_names()
     tables = inspector.get_table_names(schema=schemas[0])
-    columns = inspector.get_columns(tables[0])
+    current_table = tables[0]
 
     table_params = {
         'header_row': True,
@@ -18,8 +18,29 @@ def main(connection_string: str):
         'policy': dpg.mvTable_SizingFixedFit, 'resizable': True, 'no_host_extendX': True
     }
 
+    def select_table(sender, value):
+        tag = 'current table'
+        dpg.delete_item(tag, children_only=True)
+        table = dpg.get_value('table list')
+        columns = inspector.get_columns(table)
+        for c in columns:
+            dpg.add_table_column(label=c['name'], parent=tag)
+        with engine.connect() as conn:
+            for row in conn.execute(f'select * from {table} limit 10'):
+                with dpg.table_row(parent=tag):
+                    for e in row:
+                        dpg.add_text(e)
+
+
+    def combo_callback(sender, value):
+        dpg.configure_item('table list', items=inspector.get_table_names(schema=value))
+
     dpg.create_context()
     dpg.create_viewport(title='Seeqler', width=800, height=500)
+
+    with dpg.item_handler_registry(tag="table list handler") as handler:
+        dpg.add_item_clicked_handler(callback=select_table)
+
 
     with dpg.font_registry():
         default_font = dpg.add_font(Path(__file__).parent.parent / 'resources'/ 'FiraMono-Regular.ttf', 16)
@@ -28,24 +49,12 @@ def main(connection_string: str):
         dpg.bind_font(default_font)
 
         with dpg.group(horizontal=True):
-            with dpg.table(**table_params):
-                dpg.add_table_column(label='Schemas')
-                for row in schemas:
-                    with dpg.table_row():
-                        dpg.add_text(row)
-            with dpg.table(**table_params):
-                dpg.add_table_column(label='Tables')
-                for row in tables:
-                    with dpg.table_row():
-                        dpg.add_text(row)
-            with dpg.table(**table_params):
-                for c in columns:
-                    dpg.add_table_column(label=c['name'])
-                with engine.connect() as conn:
-                    for row in conn.execute(f'select * from {tables[0]} limit 10'):
-                        with dpg.table_row():
-                            for e in row:
-                                dpg.add_text(e)
+            with dpg.group():
+                dpg.add_combo(schemas, tag='schema selector', callback=combo_callback)
+                dpg.add_listbox((), tag='table list')
+                dpg.add_table(tag='current table', **table_params)
+
+    dpg.bind_item_handler_registry("table list", "table list handler")
 
     dpg.setup_dearpygui()
     dpg.show_viewport()
