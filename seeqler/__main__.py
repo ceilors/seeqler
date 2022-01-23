80f729fd29f4d03b53e4fe78b1cde3d45ead7ca5
Vova Abdrakhmanov <369565@gmail.com>
2022-01-23 22:56:51 +0300

    refactor: object-oriented way
    - create application class
    - define callbacks as methods
    - rewrite all about table schema

--- seeqler/__main__.py
+++ seeqler/__main__.py
@@ -6,89 +6,92 @@ import sqlalchemy as sa
 import dearpygui.dearpygui as dpg
 
 
-tag_listbox = 'table list'
-tag_content = 'current table'
-tag_schema = 'current schema'
-tag_handler = 'table list handler'
-tag_limit = 'select_limit'
-id_window = 'main_window'
-
-
-def main(connection_string: str):
-    engine = sa.create_engine(connection_string)
-    inspector = sa.inspect(engine)
-    schemas = inspector.get_schema_names()
-    tables = inspector.get_table_names(schema=schemas[0])
-    current_table = tables[0]
-
+class Seeqler:
+    tag_schema_selector = 'schema selector'
+    tag_listbox = 'table list'
+    tag_content = 'current table'
+    tag_schema = 'current schema'
+    tag_handler = 'table list handler'
+    tag_limit = 'select_limit'
+    id_window = 'main_window'
     table_params = {
         'header_row': True, 'borders_outerH': True, 'borders_innerV': True, 'borders_innerH': True,
         'borders_outerV': True, 'resizable': True, 'no_host_extendX': True
     }
 
-    def select_table(sender, value):
-        dpg.delete_item(tag_content, children_only=True)
-        dpg.delete_item(tag_schema, children_only=True)
+    def __init__(self, connection_string: str) -> 'Seeqler':
+        self.engine = sa.create_engine(connection_string)
+        self.inspector = sa.inspect(self.engine)
 
-        table = dpg.get_value(tag_listbox)
-        limit = dpg.get_value(tag_limit)
+    def ui_select_table(self, sender, table=None):
+        dpg.delete_item(self.tag_content, children_only=True)
+        dpg.delete_item(self.tag_schema, children_only=True)
 
-        columns = inspector.get_columns(table)
+        schema = dpg.get_value(self.tag_schema_selector)
+        table = table or dpg.get_value(self.tag_listbox)
+        limit = dpg.get_value(self.tag_limit)
+
+        columns = self.inspector.get_columns(table, schema=schema)
         for c in columns:
-            dpg.add_table_column(label=c['name'], parent=tag_content)
-        for column in ['param', 'type']:
-            dpg.add_table_column(label=column, parent=tag_schema)
+            dpg.add_table_column(label=c['name'], parent=self.tag_content)
+        for column in ['param', 'type', 'nullable', 'default', 'foreign key']:
+            dpg.add_table_column(label=column, parent=self.tag_schema)
 
-        with engine.connect() as conn:
+        with self.engine.connect() as conn:
             for row in conn.execute(f'select * from {table} limit {limit}'):
-                with dpg.table_row(parent=tag_content):
+                with dpg.table_row(parent=self.tag_content):
                     for e in row:
                         dpg.add_text(e)
 
+            foreign_keys = {
+                i['constrained_columns'][0]: '{referred_schema}.{referred_table}({referred_columns[0]})'.format(**i)
+                for i in self.inspector.get_foreign_keys(table, schema=schema)
+            }
             for item in columns:
-                with dpg.table_row(parent=tag_schema):
+                with dpg.table_row(parent=self.tag_schema):
                     dpg.add_text(item['name'])
                     dpg.add_text(item['type'])
+                    dpg.add_text(item['nullable'])
+                    dpg.add_text(item['default'])
+                    dpg.add_text(foreign_keys.get(item['name']))
 
+    def ui_select_schema(self, sender, schema):
+        dpg.configure_item(self.tag_listbox, items=sorted(self.inspector.get_table_names(schema=schema)))
+        self.ui_select_table(sender) # because listbox has selected item, but doesnt trigger callback itself
 
-    def combo_callback(sender, value):
-        dpg.configure_item(tag_listbox, items=inspector.get_table_names(schema=value))
+    def run(self):
+        schemas = self.inspector.get_schema_names()
+        dpg.create_context()
 
-    dpg.create_context()
-    dpg.create_viewport(title='Seeqler', width=800, height=500)
+        with dpg.font_registry():
+            default_font = dpg.add_font(Path(__file__).parent.parent / 'resources'/ 'FiraMono-Regular.ttf', 16)
 
-    with dpg.item_handler_registry(tag=tag_handler) as handler:
-        dpg.add_item_clicked_handler(callback=select_table)
+        with dpg.window(label='Window', id=self.id_window):
+            dpg.bind_font(default_font)
 
-    with dpg.font_registry():
-        default_font = dpg.add_font(Path(__file__).parent.parent / 'resources'/ 'FiraMono-Regular.ttf', 16)
+            with dpg.group(horizontal=True):
+                with dpg.group(width=200):
+                    dpg.add_text('Schemas')
+                    dpg.add_combo(schemas, tag=self.tag_schema_selector, callback=self.ui_select_schema)
+                    dpg.add_text('Tables')
+                    dpg.add_listbox((), tag=self.tag_listbox, callback=self.ui_select_table)
+                    with dpg.group():
+                        dpg.add_text('Limit')
+                        dpg.add_input_text(tag='select_limit', default_value='10')
+                with dpg.tab_bar(label='tabs'):
+                    with dpg.tab(label='content'):
+                        dpg.add_table(tag=self.tag_content, **self.table_params)
+                    with dpg.tab(label='schema'):
+                        dpg.add_table(tag=self.tag_schema, **self.table_params)
 
-    with dpg.window(label='Window', id=id_window):
-        dpg.bind_font(default_font)
-
-        with dpg.group(horizontal=True):
-            with dpg.group(width=200):
-                dpg.add_text('Schemas')
-                dpg.add_combo(schemas, tag='schema selector', callback=combo_callback)
-                dpg.add_text('Tables')
-                dpg.add_listbox((), tag=tag_listbox)
-                with dpg.group():
-                    dpg.add_text('Limit')
-                    dpg.add_input_text(tag='select_limit', default_value='10')
-            with dpg.tab_bar(label='tabs'):
-                with dpg.tab(label='content'):
-                    dpg.add_table(tag=tag_content, **table_params)
-                with dpg.tab(label='schema'):
-                    dpg.add_table(tag=tag_schema, **table_params)
-
-    dpg.bind_item_handler_registry(tag_listbox, tag_handler)
-
-    dpg.set_primary_window(id_window, True)
-    dpg.setup_dearpygui()
-    dpg.show_viewport()
-    dpg.start_dearpygui()
-    dpg.destroy_context()
+        dpg.create_viewport(title='Seeqler', width=800, height=500)
+        dpg.set_primary_window(self.id_window, True)
+        dpg.setup_dearpygui()
+        dpg.show_viewport()
+        dpg.start_dearpygui()
+        dpg.destroy_context()
 
 
 if __name__ == '__main__':
-    main(sys.argv[1])
+    app = Seeqler(sys.argv[1])
+    app.run()
