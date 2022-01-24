0ab1fc57a23792f8fc74991356876a31d5ff5a18
Vova Abdrakhmanov <369565@gmail.com>
2022-01-24 20:49:58 +0300

    feat: store your connections with connection manager

---
+++ seeqler/connection_manager.py
@@ -0,0 +1,50 @@
+import json
+import uuid
+from pathlib import Path
+from dataclasses import dataclass, asdict, field
+
+
+@dataclass
+class Connection:
+    uuid: str = field(default_factory=lambda: str(uuid.uuid4()))
+    label: str
+    connection_string: str
+
+
+default_path = Path.home() / '.config' / 'seeqler' / 'connections.json'
+
+
+class ConnectionManager:
+    def __init__(self, path: Path = default_path):
+        self.path = path
+        if not self.path.exists():
+            self.path.parent.mkdir(parents=True, exist_ok=True)
+            self.path.open('w').write('[]')
+
+    def list(self) -> List[Connection]:
+        data = json.load(self.path.open())
+        return [Connection(**i) for i in data]
+
+    def add(self, connection: Connection) -> None:
+        with self.path.open('rw') as f:
+            data = json.load(f)
+            data.append(asdict(connection))
+            json.dump(data, f)
+
+    def update(self, connection: Connection) -> None:
+        with self.path.open('rw') as f:
+            data = json.load(f)
+            for i, c in enumerate(data):
+                if c['uuid'] == connection.uuid:
+                    data[i] = asdict(connection)
+                    break
+            json.dump(data, f)
+
+    def remove(self, connection: Connection) -> None:
+        with self.path.open('rw') as f:
+            data = json.load(f)
+            for i, c in enumerate(data):
+                if c['uuid'] == connection.uuid:
+                    del data[i]
+                    break
+            json.dump(data, f)
