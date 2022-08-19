8d42d731d194886a58522480097a372396e9bbe9
_illaech <illaech@gmail.com>
2022-08-19 17:23:13 +0300

    fix: `Language` has fallback to en-us

--- seeqler/ui/language.py
+++ seeqler/ui/language.py
@@ -2,11 +2,23 @@ import importlib
 
 
 class Language:
-    # TODO: recreate as default_dict with custom descriptor
-
     def __init__(self, lang: str):
-        module = importlib.import_module(f"resources.lang_{lang.lower()}")
+        self.lang = lang
+        self.errors = ""
+
+        try:
+            module = importlib.import_module(f"resources.lang_{lang.lower()}")
+        except ModuleNotFoundError:
+            module = importlib.import_module(f"resources.lang_en-us")
+            self.errors = f'Language module for "{self.lang}" was not found'
+            self.lang = "en-us"
+
         self.__dict__.update(module.__dict__)
 
     def __getattr__(self, name):
-        return self.__dict__.get(name, name)
+        # return "$value" if value is not in language module
+        # prefix is used to distinguish translation absence clearly
+        return self.__dict__.get(name, f"${name}")
+
+    def get(self, name):
+        return self.__getattr__(name)
